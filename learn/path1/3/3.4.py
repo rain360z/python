1137ba23d7265e2f2ce23f5ca76d4bc0e3b107f4
Rain360z <lexe16231@gmail.com>
2025-05-15 12:43:58 +0000

    new file:   learn/path1/3/3.4.py new file:   path1/3/3.4.py

---
+++ learn/path1/3/3.4.py
@@ -0,0 +1,8 @@
+guests = ['grandmather','grandfather','grandmother my wife']
+
+
+print(f"Hello, dearie {guests[0]}. I invite you on drink tea ")
+print(f"\nHello, dearie {guests[1]}. I invite you on drink tea ")
+print(f"\nHello, dearie {guests[2]}. I invite you on drink tea ")
+
+
