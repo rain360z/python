8858e7682d6b30b5706ee7f29baa817b0d80f347
Rain360z <lexe16231@gmail.com>
2026-05-03 16:23:02 +0400

    Change structure

---
+++ learn/path1/6/alien.py
@@ -0,0 +1,5 @@
+alien_0 = {'color': 'green', 'point': '5'}
+
+print(alien_0['color'])
+print(alien_0['point'])
+
---
+++ python_project/Command for Add Vlan/Test.py
@@ -0,0 +1,7 @@
+New_vlan={'name':'vlan_333','Description':'For IB', 'ID':'333'}
+Checkpoint_int = ['po33','po34']
+
+
+Cisco140_1_ksib_before={'Chekpoint_int':Checkpoint_int, 'ps140_1':'' }
+
+print(Cisco140_1_ksib_before['Chekpoint_int'])
