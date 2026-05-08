7803ee4e53562e42972a637d36bcabdc937307da
Rain360z <lexe16231@gmail.com>
2026-05-08 15:41:17 +0400

    write code alien.py. Move alien position

--- learn/path1/6/alien.py
+++ learn/path1/6/alien.py
@@ -3,8 +3,28 @@ alien_0 = {'color': 'green', 'point': '5'}
 print(alien_0['color'])
 print(alien_0['point'])
 
+print("List dictinary")
+print(alien_0)
 # Add new object
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 
+print("List dictinary after add 2 key")
 print(alien_0)
+
+print('Add speed alien')
+alien_0['speed']= 'medium'
+print(alien_0)
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    #So fast
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New Position: {alien_0['x_position']}")
+
